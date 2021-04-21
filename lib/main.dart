a92ef8896855ac00b25b71eb62a979a91cce36ea
michaelbarseghian <michael.barseghian@gmail.com>
2021-04-20 18:38:15 -0700

    updated google API uncommented imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-//import 'package:flutter/material.dart' hide Router;
-//import 'Routing/routing.dart';
-//import 'Routing/routing_constants.dart';
+import 'package:flutter/material.dart' hide Router;
+import 'Routing/routing.dart';
+import 'Routing/routing_constants.dart';
 
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:crave/blocs/auth_bloc.dart';
 import 'package:crave/screens/login.dart';
