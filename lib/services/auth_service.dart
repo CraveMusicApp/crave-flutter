c29daf492f8274609a0eba6aa1796909dcb2a6e5
michaelbarseghian <michael.barseghian@gmail.com>
2021-05-04 20:14:48 -0700

    RetrievesGoogleProfileInfo

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,10 +1,23 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 
 class AuthService {
-  final _auth = FirebaseAuth.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  Future<UserCredential> signInWithCredential(AuthCredential credential) => 
-  _auth.signInWithCredential(credential);
+  Future<UserCredential> signInWithCredential(AuthCredential credential) =>
+      _auth.signInWithCredential(credential);
   Future<void> logout() => _auth.signOut();
   Stream<User> get currentUser => _auth.authStateChanges();
+
+  Future getCurrentUser() async {
+    return _auth.currentUser;
+  }
+
+  getProfileImage() {
+    if (_auth.currentUser.photoURL != null) {
+      return Image.network(_auth.currentUser.photoURL);
+    } else {
+      return Icon(Icons.account_circle, size: 100);
+    }
+  }
 }
